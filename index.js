4a045fd8822fd089647b9267dba1dedfbf200083
Shwetha K <shweetha.sweety17@gmail.com>
2025-05-09 19:38:52 +0530

    Final Code

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static("public"));
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
-
-app.get("/api/:date",(req,resp)=>{
-   const date = req.params['date'];
-   if(!date){
-    
-    return resp.json({ error : "Invalid Date" })
-   }
-
-  if(isNaN(date)){
-    return resp.json({'unix':new Date(Date.now()).getTime()})
+// get the date
+app.get("/api/:date", (req, resp) => {
+  let responseObject = {};
+  if (Date.parse(req.params.date)) {
+    let date = req.params.date;
+    responseObject["unix"] = new Date(date).getTime();
+    responseObject["utc"] = new Date(date).toUTCString();
+  } else if (Number.parseInt(req.params.date)) {
+    let date = req.params.date;
+    responseObject["unix"] = Number(date);
+    responseObject["utc"] = new Date(Number(date)).toUTCString();
+  } else {
+    responseObject["error"] = "Invalid Date";
   }
-   const unix = new Date(date).getTime()/1000
-  
-   resp.json({'unix':unix});
 
-  
-})
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  resp.json(responseObject);
 });
+// present date
+app
+  .get("/api", (req, resp) => {
+    let responseObject = {};
+    responseObject["unix"] = new Date().getTime();
+    responseObject["utc"] = new Date().toUTCString();
+    return resp.json(responseObject);
+  })
 
+  .app.get("/api/hello", function (req, res) {
+    res.json({ greeting: "hello API" });
+  });
 
-
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.listen(process.env.PORT);
